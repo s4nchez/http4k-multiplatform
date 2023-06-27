053d699e2a348f0b16faaeb7724258bfed11c2fd
Ivan Sanchez <ivan@gourame.com>
2023-06-27 22:17:43 +0100

    Add dummy actuals in other plaforms

---
+++ src/nativeMain/kotlin/org/http4k/core/BodyData.kt
@@ -0,0 +1,11 @@
+package org.http4k.core
+
+actual object BodyData {
+    actual fun of(array: ByteArray): DataInMemory {
+        TODO("Not yet implemented")
+    }
+
+    actual fun of(string: String): DataInMemory {
+        TODO("Not yet implemented")
+    }
+}
