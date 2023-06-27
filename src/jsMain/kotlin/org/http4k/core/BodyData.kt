a7c82ab1e2aeb6ac1f5cacbf15dbfd3c08113409
Ivan Sanchez <ivan@gourame.com>
2023-06-27 23:13:53 +0100

    Naive js impl

--- src/jsMain/kotlin/org/http4k/core/BodyData.kt
+++ src/jsMain/kotlin/org/http4k/core/BodyData.kt
@@ -1,27 +1,18 @@
 package org.http4k.core
 
-actual class DataStream : Closeable {
-    actual fun consumeAll(): DataInMemory {
-        TODO("Not yet implemented")
-    }
+actual data class DataStream(val value: String) : Closeable {
+    actual fun consumeAll(): DataInMemory = DataInMemory(value)
 
     override fun close() {
-        TODO("Not yet implemented")
     }
 }
 
-actual class DataInMemory actual constructor(value: String) {
-    actual fun asStream(): DataStream {
-        TODO("Not yet implemented")
-    }
+actual data class DataInMemory actual constructor(val value:String)  {
+    actual fun asStream(): DataStream = DataStream(value)
 
-    actual fun asString(): String {
-        TODO("Not yet implemented")
-    }
+    actual fun asString(): String = value
 
-    actual fun size(): Int {
-        TODO("Not yet implemented")
-    }
+    actual fun size(): Int = value.length
 
     actual constructor(value: ByteArray) : this("") {
         TODO("Not yet implemented")
