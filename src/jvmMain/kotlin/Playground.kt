93e5dca2705b646182a3edb866da1fae856657f5
Ivan Sanchez <ivan@gourame.com>
2023-06-27 23:08:07 +0100

    Add Playground.kt

---
+++ src/jvmMain/kotlin/Playground.kt
@@ -0,0 +1,17 @@
+import org.http4k.client.JavaHttpClient
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+
+fun main() {
+    val server = { request: Request -> Response(Status.OK).body("Hello, world!") }
+
+    server.asServer(SunHttp(9000)).start()
+
+    val response = JavaHttpClient()(Request(Method.GET, "http://localhost:9000"))
+
+    println(response)
+}
