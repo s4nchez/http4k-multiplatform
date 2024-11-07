803960050db53e47efdbdeabdaa907342ed6a6f8
Ivan Sanchez <ivan@gourame.com>
2024-11-07 15:42:48 +0000

    Add libcurl def and Main example

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform") version "1.9.0"
 }
@@ -19,14 +21,36 @@ kotlin {
             useJUnitPlatform()
         }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
 
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+
+    nativeTarget.apply {
+        compilations.getByName("main") {
+            cinterops {
+                val libcurl by creating {
+                    defFile = project.file("src/nativeInterop/cinterop/libcurl.def")
+                    packageName("com.jetbrains.handson.http")
+                    compilerOpts("-I/path")
+                    includeDirs.allHeaders("path")
+                }
+            }
+        }
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
     }
 
     sourceSets {
@@ -47,7 +71,12 @@ kotlin {
             }
         }
         val jvmTest by getting
-        val nativeMain by getting
+        val nativeMain by getting {
+            dependencies {
+                api("org.jetbrains.kotlin:kotlin-stdlib-common")
+            }
+        }
         val nativeTest by getting
     }
+
 }
---
+++ src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,19 @@
+@file:Suppress("UNUSED_PARAMETER")
+
+import com.jetbrains.handson.http.*
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.toKString
+
+@OptIn(ExperimentalForeignApi::class)
+fun main(args: Array<String>) {
+    val curl = curl_easy_init()
+    if (curl != null) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://example.com")
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)
+        val res = curl_easy_perform(curl)
+        if (res != CURLE_OK) {
+            println("curl_easy_perform() failed ${curl_easy_strerror(res)?.toKString()}")
+        }
+        curl_easy_cleanup(curl)
+    }
+}
